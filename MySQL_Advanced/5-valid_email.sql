c7dfd40aee049623bb50761d32fa6a23e18a7b4a
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-03-15 15:29:26 +0000

    fix

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,9 @@
--- Create a trigger that resets valid_email when the email is changed
+-- Reset valid_email only when the email is changed
+-- If the email remains the same, valid_email should not change
 
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-SET NEW.valid_email = IF(OLD.email <> NEW.email, 0, NEW.valid_email);
+IF OLD.email <> NEW.email THEN
+    SET NEW.valid_email = 0;
+END IF;
