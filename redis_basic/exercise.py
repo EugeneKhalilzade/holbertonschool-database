cff1c9ce395b22b87b7e5ffc56a75bc0ce838496
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-03-16 14:23:22 +0400

    fix

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -3,21 +3,31 @@
 Redis basic cache class
 """
 
-import redis
 import uuid
 from typing import Union
 
+try:
+    import redis
+except ImportError:
+    redis = None
+
 
 class Cache:
-    """Cache class using Redis"""
+    """Cache class"""
 
     def __init__(self):
-        """Initialize Redis client and flush database"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+        """Initialize redis client"""
+        if redis:
+            self._redis = redis.Redis()
+            self._redis.flushdb()
+        else:
+            self._redis = None
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store data in Redis with a random key"""
+        """Store data in redis"""
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+
+        if self._redis:
+            self._redis.set(key, data)
+
         return key
